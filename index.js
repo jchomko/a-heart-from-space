2cee67ebd6e326452007d2ca2aa444e98865741e
Jonathan Chomko <jonathan.chomko@gmail.com>
2019-08-15 12:25:29 -0400

    updating via script

--- index.js
+++ index.js
@@ -31,81 +31,50 @@ io.on('connection', function(socket){
   //Add client id to usersList if on mobile
   socket.on("new-client", function(data){
 
-    if (data == "mobile"){
+    // if (data == "mobile"){
+    //
+    //     if(usersList.indexOf(this.id) == -1){
+    //       usersList.push( this.id )
+    //       console.log("New Mobile Client: ", this.id," - updated list: ", usersList)
+    //       //respond with their their id
+    //       io.to(this.id).emit("client-id", usersList.indexOf(this.id))
+    //     }else{
+    //       console.log("Existing Mobile Client: ", this.id," - updated list: ", usersList)
+    //   }
+    //     //if the user is a debug viewer add them to a separate list
+    //   }else if (data == 'debug'){
+    //     if (debugList.indexOf(this.id) == -1){
+    //        debugList.push(this.id)
+    //        console.log("New Debug Client: ", this.id," - updated list: ", debugList)
+    //     }else{
+    //       console.log("Existing Debug Client: ", this.id," - updated list: ", debugList)
+    //      }
+    //   }
 
-        if(usersList.indexOf(this.id) == -1){
-          usersList.push( this.id )
-          console.log("New Mobile Client: ", this.id," - updated list: ", usersList)
-          //respond with their their id
-          io.to(this.id).emit("client-id", usersList.indexOf(this.id))
-        }else{
-          console.log("Existing Mobile Client: ", this.id," - updated list: ", usersList)
-      }
-        //if the user is a debug viewer add them to a separate list
-      }else if (data == 'debug'){
-        if (debugList.indexOf(this.id) == -1){
-           debugList.push(this.id)
-           console.log("New Debug Client: ", this.id," - updated list: ", debugList)
-        }else{
-          console.log("Existing Debug Client: ", this.id," - updated list: ", debugList)
-         }
-      }
   })
 
   //this happens automatically when the socket connection breaks
   socket.on('disconnect', function(){
     //Remove mobile client from list
-    var disconnectedClientIndex = usersList.indexOf(this.id)
-    if (disconnectedClientIndex >= 0){
-      usersList.splice(disconnectedClientIndex, 1)
-      console.log("Mobile Client disconnected: ", this.id," - updated list: ", usersList)
-    }
-
-    //Remove debug client from list
-    var disconnectedDebugClientIndex = debugList.indexOf(this.id)
-    if (disconnectedDebugClientIndex >= 0){
-      debugList.splice(disconnectedDebugClientIndex, 1)
-      console.log("Debug Client disconnected: ", this.id," - updated list: ", debugList)
-    }
-
-    var exists = false
-    var index = -1
-
-    for( var i=0; i < groupCoords.length; i ++){
-      //if we find a match
-      if(groupCoords[i].id === this.id){
-          groupCoords[i].lat = coords.lat
-          groupCoords[i].lng = coords.lng
-          exists = true
-          index = i
-       }
-     }
-
-     //if we have a match
-     //remove that match from the list of coordinates
-     if(exists == true){
-       groupCoords.splice(index, 1)
-     }
-
-  })
-
-  //This we will call when we want a device to go offline
-  socket.on('removeclient', function(){
     // var disconnectedClientIndex = usersList.indexOf(this.id)
     // if (disconnectedClientIndex >= 0){
     //   usersList.splice(disconnectedClientIndex, 1)
-    //   console.log("removing client from list of active users: ", this.id," - updated list: " + usersList)
+    //   console.log("Mobile Client disconnected: ", this.id," - updated list: ", usersList)
+    // }
+    //
+    // //Remove debug client from list
+    // var disconnectedDebugClientIndex = debugList.indexOf(this.id)
+    // if (disconnectedDebugClientIndex >= 0){
+    //   debugList.splice(disconnectedDebugClientIndex, 1)
+    //   console.log("Debug Client disconnected: ", this.id," - updated list: ", debugList)
     // }
 
     var exists = false
     var index = -1
 
     for( var i=0; i < groupCoords.length; i ++){
-
       //if we find a match
       if(groupCoords[i].id === this.id){
-          groupCoords[i].lat = coords.lat
-          groupCoords[i].lng = coords.lng
           exists = true
           index = i
        }
@@ -117,10 +86,41 @@ io.on('connection', function(socket){
        groupCoords.splice(index, 1)
      }
 
-    }
-
-
+     console.log("removed eleemnt" + groupCoords)
   })
+
+  // //This we will call when we want a device to go offline
+  // socket.on('removeclient', function(){
+  //   // var disconnectedClientIndex = usersList.indexOf(this.id)
+  //   // if (disconnectedClientIndex >= 0){
+  //   //   usersList.splice(disconnectedClientIndex, 1)
+  //   //   console.log("removing client from list of active users: ", this.id," - updated list: " + usersList)
+  //   // }
+  //
+  //   var exists = false
+  //   var index = -1
+  //
+  //   for( var i=0; i < groupCoords.length; i ++){
+  //
+  //     //if we find a match
+  //     if(groupCoords[i].id === this.id){
+  //         groupCoords[i].lat = coords.lat
+  //         groupCoords[i].lng = coords.lng
+  //         exists = true
+  //         index = i
+  //      }
+  //    }
+  //
+  //    //if we have a match
+  //    //remove that match from the list of coordinates
+  //    if(exists == true){
+  //      groupCoords.splice(index, 1)
+  //    }
+  //
+  //
+  //
+  //
+  // })
 
   socket.on('addclient', function(){
     if (usersList.indexOf(this.id) == -1){
